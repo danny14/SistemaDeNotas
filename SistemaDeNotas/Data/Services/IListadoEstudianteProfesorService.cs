967224176ce8beea8bdf368c577e0bf0616def94
Danny <danny_ruiz_1995@hotmail.com>
2021-05-14 18:43:30 -0500

    Creando el modelo para obtener los estudiantes que tienen la nota baja

---
+++ SistemaDeNotas/Data/Services/IListadoEstudianteProfesorService.cs
@@ -0,0 +1,11 @@
+﻿using SistemaDeNotas.Data.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SistemaDeNotas.Data.Services
+{
+    public interface IListadoEstudianteProfesorService
+    {
+        Task<IEnumerable<ListadoEstudianteProfesor>> GetAllEstudiantesDelProfesorA(int id);
+    }
+}
